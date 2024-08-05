d6530f1a617abe8d6605bfacc0eca5b8eddfbdcf
murasame29 <oogiriminister@gmail.com>
2024-08-05 22:11:17 +0900

    fix: env

--- push-notify-app/cmd/config/config.go
+++ push-notify-app/cmd/config/config.go
@@ -5,11 +5,11 @@ import "github.com/caarlos0/env/v11"
 func LoadEnv() error {
 	config := config{}
 
-	if err := env.Parse(config.App); err != nil {
+	if err := env.Parse(&config.App); err != nil {
 		return err
 	}
 
-	if err := env.Parse(config.GitHub); err != nil {
+	if err := env.Parse(&config.GitHub); err != nil {
 		return err
 	}
 
